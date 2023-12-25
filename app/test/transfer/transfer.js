f40407cc26a9ff71acfffd022c21f69b461bc834
mfckinstarboy <emmanuellmartinez013@gmail.com>
2023-12-25 09:11:56 +0800

    enhance transfer form styling

--- app/test/transfer/transfer.js
+++ app/test/transfer/transfer.js
@@ -26,6 +26,7 @@ function showInternalTransferForm() {
   document.getElementById("external-transfer-form").style.display = "none";
   document.querySelector(".internal-transfer-desc").style.display = "block";
   document.querySelector(".external-transfer-desc").style.display = "none";
+  switchForm("internal-transfer-form");
 }
 
 function showExternalTransferForm() {
@@ -35,6 +36,7 @@ function showExternalTransferForm() {
   document.getElementById("external-transfer-form").style.display = "block";
   document.querySelector(".internal-transfer-desc").style.display = "none";
   document.querySelector(".external-transfer-desc").style.display = "block";
+  switchForm("external-transfer-form");
 }
 
 function setDefaultActiveButton() {
@@ -45,3 +47,23 @@ function setDefaultActiveButton() {
 
 // Call the function when the page loads
 window.onload = setDefaultActiveButton;
+
+/* smooth transition for transfer options selection */
+function switchForm(formId) {
+  // Hide all forms
+  let forms = document.querySelectorAll(".transfer-form-wrapper");
+  forms.forEach(function (form) {
+    form.classList.remove("show-form");
+  });
+
+  // Show the selected form after a small delay for the fade-out effect
+  setTimeout(function () {
+    let selectedForm = document.getElementById(formId);
+    selectedForm.classList.add("show-form");
+  }, 50);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  let transferContainer = document.querySelector(".apex-transfer-container");
+  transferContainer.classList.add("fade-in");
+});
