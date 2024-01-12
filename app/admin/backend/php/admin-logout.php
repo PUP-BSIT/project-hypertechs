a43a1ef34271ac6cf739d5d01ab00f5afc6b8d18
danay_maryjoy <maryjoydanay2.0@gmail.com>
2024-01-12 08:21:12 +0800

    modify log out

---
+++ app/admin/backend/php/admin-logout.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+$ONE_DAY = 60*60*24;
+
+unset($_SESSION['admin_number']);
+unset($_SESSION['phone_number']);
+$params = session_get_cookie_params();
+setcookie(session_name(), "", time() - $ONE_DAY, $params['path'], 
+        $params['domain'], $params['secure'], $params['httponly']);
+session_destroy();
+$response['success'] = true;
+echo json_encode($response);
+?>
