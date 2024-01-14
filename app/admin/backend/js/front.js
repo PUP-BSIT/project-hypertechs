4dff2d15b384eb6638f5b513d000c9a46c565ad1
danay_maryjoy <maryjoydanay2.0@gmail.com>
2024-01-15 05:32:06 +0800

    add temporary admin front page

---
+++ app/admin/backend/js/front.js
@@ -0,0 +1,26 @@
+import { postData, getData, isLoggedIn } from "./common_new.js";
+
+const ID_BTN_LOGIN = "#button_login";
+const URL_ACCOUNT = "./app/admin/pages/admin_account/overview.html";
+const URL_LOGIN = "./app/admin/pages/login.html";
+
+main();
+
+function main() {
+  let btnLogin;
+
+  btnLogin = document.querySelector(ID_BTN_LOGIN);
+  btnLogin.addEventListener("click", checkSession);
+}
+
+async function checkSession() {
+  let loggedIn;
+
+  loggedIn = await isLoggedIn();
+  console.log(loggedIn);
+  if (loggedIn) {
+    window.location.href = URL_ACCOUNT;
+    return;
+  }
+  window.location.href = URL_LOGIN;
+}
