991bdf7ecc09fb70070345263035d689655f564c
Calib Serrano <sca@outlook.ph>
2024-01-15 22:36:59 +0800

    fix CORS

--- app/client/backend/php/receive-external-transfer.php
+++ app/client/backend/php/receive-external-transfer.php
@@ -1,9 +1,9 @@
 <?php
 require "./common.php";
 
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: *");
-header("Access-Control-Allow-Headers: *");
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Methods:*");
+header("Access-Control-Allow-Headers:*");
 header("Access-Control-Allow-Credentials: true");
 
 connect_database();
