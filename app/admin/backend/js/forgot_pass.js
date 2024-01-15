b67b07f70e7657d6efab4e3267a9d2948b9f5123
danay_maryjoy <maryjoydanay2.0@gmail.com>
2024-01-16 05:49:26 +0800

    change url

--- app/admin/backend/js/forgot_pass.js
+++ app/admin/backend/js/forgot_pass.js
@@ -1,6 +1,4 @@
-import {
-        postData, getData, saveRequest
-} from "./common_new.js";
+import { postData, getData, saveRequest } from "./common_new.js";
 
 const ID_PHONE_NUMBER = "#account_phone";
 const ID_SUBMIT = "#forgot_button";
@@ -8,24 +6,24 @@ const ID_SUBMIT = "#forgot_button";
 main();
 
 function main() {
-        let btnSubmit;
+  let btnSubmit;
 
-        btnSubmit = document.querySelector(ID_SUBMIT);
-        btnSubmit.addEventListener("click", requestChangePass);
+  btnSubmit = document.querySelector(ID_SUBMIT);
+  btnSubmit.addEventListener("click", requestChangePass);
 }
 
 async function requestChangePass() {
-        let phoneNumber, requestBody, url, response;
+  let phoneNumber, requestBody, url, response;
 
-        phoneNumber = document.querySelector(ID_PHONE_NUMBER).value;
-        requestBody = new FormData();
-        requestBody.append('phone_number', phoneNumber);
-        url = "/app/client/backend/php/forgot-pass-check.php";
-        response = await postData(url, requestBody); 
-        if (!response.success) {
-                alert("Account does not exist");
-                return;
-        }
-        url = "/app/client/backend/php/forgot-pass.php";
-        await saveRequest(url, requestBody);
+  phoneNumber = document.querySelector(ID_PHONE_NUMBER).value;
+  requestBody = new FormData();
+  requestBody.append("phone_number", phoneNumber);
+  url = "/app/admin/backend/php/forgot-pass-check.php";
+  response = await postData(url, requestBody);
+  if (!response.success) {
+    alert("Account does not exist");
+    return;
+  }
+  url = "/app/admin/backend/php/forgot-pass.php";
+  await saveRequest(url, requestBody);
 }
