efcac90c3ec1b31a37d053ca10da5b6c3993676a
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-01-16 08:05:45 +0800

    fix transfer amount validation

--- app/client/backend/js/fund_transfer.js
+++ app/client/backend/js/fund_transfer.js
@@ -47,8 +47,9 @@ async function requestTransfer() {
                 alert("Account number should contain exactly 12 digits.");
                 return;
         }
-        if (!/^\d{1,6}$/.test(amount)) {
-                alert("Amount should be limited to six digits only.");
+        if (!/^\d{1,6}(\.\d{2})?$/.test(amount)) {
+                alert("Amount should be up to six digits\n" +
+                        "with exactly two decimal places.");
                 return;
         }
         requestBody = new FormData();
--- app/client/backend/js/fund_transfer_external.js
+++ app/client/backend/js/fund_transfer_external.js
@@ -45,8 +45,9 @@ async function requestTransfer() {
                 alert("Please fill out all the required fields.");
                 return;
         }
-        if (!/^\d{1,6}$/.test(amount)) {
-                alert("Amount should be limited to six digits only.");
+        if (!/^\d{1,6}(\.\d{2})?$/.test(amount)) {
+                alert("Amount should be up to six digits\n" +
+                        "with exactly two decimal places.");
                 return;
         }
         requestBody = new FormData();
