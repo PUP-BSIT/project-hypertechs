2a569201bb54417c38b7d36a10cefd96a7ec7a68
Calib Serrano <sca@outlook.ph>
2024-01-16 14:13:30 +0800

    show account name

--- app/client/backend/js/account_info.js
+++ app/client/backend/js/account_info.js
@@ -8,6 +8,7 @@ const ID_ACCOUNT_NAME = "#display_account_holder";
 const ID_ACCOUNT_NUMBER = "#display_account_number";
 const ID_BALANCE = "#display_account_balance";
 const ID_FIRST_NAME = "#account_first_name";
+const ID_MINI_NAME = "#display_mini_account_text";
 const HOME_URL = "/index.html";
 
 main();
@@ -42,15 +43,22 @@ async function getCustomerInfo() {
 }
 
 function showCustomerData(data) {
-        let accountName, accountNumber, balance, firstName, name;
+        let accountName, accountNumber, balance, firstName, name, miniName;
 
         firstName = document.querySelector(ID_FIRST_NAME);
-        firstName.innerHTML = data.data.firstName + "!";
+        miniName = document.querySelector(ID_MINI_NAME);
         accountName = document.querySelector(ID_ACCOUNT_NAME);
         accountNumber = document.querySelector(ID_ACCOUNT_NUMBER);
         balance = document.querySelector(ID_BALANCE); 
-        if (!accountName || !accountNumber || !balance) return;
-        accountName.innerHTML = data.data.name;
-        accountNumber.innerHTML = data.data.accountNumber;
-        balance.innerHTML = strToNum(data.data.balance); 
+        if (firstName) {
+                firstName.innerHTML = data.data.firstName + "!";
+        }
+        if (miniName) {
+                miniName.innerHTML = data.data.name;
+        }
+        if (accountName || accountNumber || balance) {
+                accountName.innerHTML = data.data.name;
+                accountNumber.innerHTML = data.data.accountNumber;
+                balance.innerHTML = strToNum(data.data.balance); 
+        }
 }
