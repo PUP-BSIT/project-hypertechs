b79b342b095f2095e123a0ed441a05448aef6a9e
danay_maryjoy <maryjoydanay2.0@gmail.com>
2024-01-17 20:18:22 +0800

    improve overview

--- app/admin/backend/js/admin_info.js
+++ app/admin/backend/js/admin_info.js
@@ -9,9 +9,10 @@ import {
 
 let ADMIN_ID;
 const ID_LOGOUT_BUTTON = "#logout";
-const ID_ADMIN_NAME = "#display_admin_holder";
+const ID_ADMIN_NAME = "#display_admin_name";
+const ID_FIRST_NAME = "#admin_first_name";
 const ID_ADMIN_NUMBER = "#display_admin_id";
-// const ID_BALANCE = "#display_admin_balance";
+// const ID_BALANCE = "#display_admin_balance";//
 
 main();
 
@@ -28,6 +29,7 @@ function startOperation() {
   // balance = document.querySelector(ID_BALANCE);
   adminName.innerHTML = "Loading...";
   adminId.innerHTML = "Loading...";
+  firstName.innerHTML = "Loading...";
   // balance.innerHTML = "Loading...";
   url = "../../backend/php/admin-session.php";
   fetchData(url, setAdminId);
@@ -54,11 +56,13 @@ function getAdminInfo() {
 function showAdminData(data) {
   console.log(data);
   if (!data.success) return;
-  let adminName, adminId;
+  let adminName, adminId, firstName;
 
+  firstName = document.querySelector(ID_FIRST_NAME);
   adminName = document.querySelector(ID_ADMIN_NAME);
   adminId = document.querySelector(ID_ADMIN_NUMBER);
   // balance = document.querySelector(ID_BALANCE);
+  firstName.innerHTML = data.data.firstName;
   adminName.innerHTML = data.data.name;
   adminId.innerHTML = data.data.adminId;
   // balance.innerHTML = strToNum(data.data.balance);
