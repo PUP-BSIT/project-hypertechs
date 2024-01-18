70afce1b62a609a36ae82d47cad50ef043160e50
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-01-18 22:09:52 +0800

    modify admin account summary

--- app/admin/src/scripts/account/overview.js
+++ app/admin/src/scripts/account/overview.js
@@ -1,27 +1,25 @@
 // display time in admin overview
 
 function updateDateTime() {
-  // Create a new Date object
-  var currentDate = new Date();
+  let currentDate = new Date();
 
-  // Get date and time components using the 'en-PH' locale
-  var dateOptions = { weekday: "long", month: "long", day: "numeric" };
-  var timeOptions = {
+  let dateOptions = { weekday: "long", month: "long", day: "numeric" };
+  let timeOptions = {
     hour: "2-digit",
     minute: "2-digit",
     second: "2-digit",
     hour12: false,
   };
 
-  var date = currentDate.toLocaleDateString("en-PH", dateOptions);
-  var time = currentDate.toLocaleTimeString("en-PH", timeOptions);
+  let date = currentDate.toLocaleDateString("en-PH", dateOptions);
+  let time = currentDate.toLocaleTimeString("en-PH", timeOptions);
 
   // Update the HTML elements
   document.getElementById("display_date").innerText = date;
   document.getElementById("display_time").innerText = time;
 }
 
-// Update the date and time every second (you can adjust the interval as needed)
+
 setInterval(updateDateTime, 1000);
 
 // Initial update when the page loads
