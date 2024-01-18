069bf9c4f5bbc6e4c2a73607f6afeb870b5d6822
Calib Serrano <sca@outlook.ph>
2024-01-18 23:07:39 +0800

    fix time

--- app/client/backend/php/common.php
+++ app/client/backend/php/common.php
@@ -45,8 +45,6 @@ function close_database() {
 
 function get_transaction_data($table, $transaction_id) {
         $transaction_id_col = "transaction_id";
-        $sql_stmt = "SET time_zone = 'Asia/Manila'";
-        modify_database($sql_stmt);
         $sql_stmt = "SELECT *, TIME_FORMAT(time, '%h:%i %p') AS timef FROM 
                 $table WHERE $transaction_id_col='$transaction_id';";
         $result = extract_database($sql_stmt);
