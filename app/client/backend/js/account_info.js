b21d267d1cad545a0317bcdd36e50b52bf842eb5
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-01-20 11:17:16 +0800

    dynamically refresh balance

--- app/client/backend/js/account_info.js
+++ app/client/backend/js/account_info.js
@@ -14,51 +14,56 @@ const HOME_URL = "/index.html";
 main();
 
 async function main() {
-        let response, loggedIn, accountName, accountNumber, balance, url;
+    let response, loggedIn, accountName, accountNumber, balance, url;
 
-        loggedIn = await isLoggedIn();
-        if (!loggedIn) {
-                window.location.href = HOME_URL;
-                return;
-        }
-        url = "/app/client/backend/php/customer-session.php";
-        response = await getData(url);
-        if (!response.success) {
-                return;
-        }
-        ACCOUNT_NUMBER = response.accountNumber;
-        getCustomerInfo();
+    loggedIn = await isLoggedIn();
+    if (!loggedIn) {
+        window.location.href = HOME_URL;
+        return;
+    }
+
+    url = "/app/client/backend/php/customer-session.php";
+    response = await getData(url);
+    if (!response.success) {
+        return;
+    }
+
+    ACCOUNT_NUMBER = response.accountNumber;
+    getCustomerInfo();
+
+    setInterval(getCustomerInfo, 1000);
 }
 
 async function getCustomerInfo() {
-        let sessionAccount, url, requestBody, response;
+    let sessionAccount, url, requestBody, response;
 
-        url = "/app/client/backend/php/customer-info.php";
-        sessionAccount = ACCOUNT_NUMBER;
-        requestBody = new FormData();
-        requestBody.append('account_number', sessionAccount);
-        response = await postData(url, requestBody);
-        if (!response.success) return;
-        showCustomerData(response);
+    url = "/app/client/backend/php/customer-info.php";
+    sessionAccount = ACCOUNT_NUMBER;
+    requestBody = new FormData();
+    requestBody.append('account_number', sessionAccount);
+    response = await postData(url, requestBody);
+    if (!response.success) return;
+    showCustomerData(response);
 }
 
 function showCustomerData(data) {
-        let accountName, accountNumber, balance, firstName, name, miniName;
+    let accountName, accountNumber, balance, firstName, name, miniName;
 
-        firstName = document.querySelector(ID_FIRST_NAME);
-        miniName = document.querySelector(ID_MINI_NAME);
-        accountName = document.querySelector(ID_ACCOUNT_NAME);
-        accountNumber = document.querySelector(ID_ACCOUNT_NUMBER);
-        balance = document.querySelector(ID_BALANCE); 
-        if (firstName) {
-                firstName.innerHTML = data.data.firstName + "!";
-        }
-        if (miniName) {
-                miniName.innerHTML = data.data.name;
-        }
-        if (accountName || accountNumber || balance) {
-                accountName.innerHTML = data.data.name;
-                accountNumber.innerHTML = data.data.accountNumber;
-                balance.innerHTML = strToNum(data.data.balance); 
-        }
+    firstName = document.querySelector(ID_FIRST_NAME);
+    miniName = document.querySelector(ID_MINI_NAME);
+    accountName = document.querySelector(ID_ACCOUNT_NAME);
+    accountNumber = document.querySelector(ID_ACCOUNT_NUMBER);
+    balance = document.querySelector(ID_BALANCE); 
+
+    if (firstName) {
+        firstName.innerHTML = data.data.firstName + "!";
+    }
+    if (miniName) {
+        miniName.innerHTML = data.data.name;
+    }
+    if (accountName || accountNumber || balance) {
+        accountName.innerHTML = data.data.name;
+        accountNumber.innerHTML = data.data.accountNumber;
+        balance.innerHTML = strToNum(data.data.balance); 
+    }
 }
