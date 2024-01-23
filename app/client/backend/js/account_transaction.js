93ecb980e2a4d81233b2e5179fd4384622ff4477
Calib Serrano <sca@outlook.ph>
2024-01-24 00:07:32 +0800

    add transaction history backend

---
+++ app/client/backend/js/account_transaction.js
@@ -0,0 +1,48 @@
+import {
+        getData, postData
+} from "./common_new.js";
+
+const ID_TRANSAC_TYPE = "#transaction-type";
+const ID_START_DATE = "#start-date";
+const ID_END_DATE = "#end-date";
+const ID_BTN_FILTER = "#btn_filter";
+
+main();
+
+async function main() {
+        let filterButton;
+
+        filterButton = document.querySelector(ID_BTN_FILTER);
+        filterButton.addEventListener("click", filterResult);
+        filterResult();
+}
+
+async function filterResult() {
+        let filterType, filterStartDate, filterEndDate, tableBody, tableRow, 
+                tableCell, data, filterButton, url, response, requestBody,
+                tableRows;
+
+        filterType = document.querySelector(ID_TRANSAC_TYPE).value;
+        filterStartDate = document.querySelector(ID_START_DATE).value;
+        filterEndDate = document.querySelector(ID_END_DATE).value;
+        console.log(filterType, filterStartDate, filterEndDate);
+
+        url = "/app/client/backend/php/account-transaction.php";
+        requestBody = new FormData();
+        requestBody.append('start_date', filterStartDate);
+        requestBody.append('end_date', filterEndDate);
+        requestBody.append('transac_type', filterType);
+        response = await postData(url, requestBody);
+        console.log(response);
+        tableBody = document.querySelector('#transaction_table tbody');
+        tableBody.innerHTML = "";
+        for (let transaction of response.data) {
+                tableRow = tableBody.insertRow();
+                for (let key in transaction) {
+                        tableCell = tableRow.insertCell();
+                        tableCell.innerHTML = transaction[key];
+                }
+        }
+}
+
+
---
+++ app/client/backend/php/account-transaction.php
@@ -0,0 +1,131 @@
+<?php
+require "./common.php";
+session_start();
+connect_database();
+
+$start_date = $_POST['start_date'];
+$end_date = $_POST['end_date'];
+$transac_type = $_POST['transac_type'];
+$account_number = $_SESSION['account_number'];
+$date_condition = "";
+if ($start_date == "" && $end_date == "") {
+        $date_condition = "";
+} else if ($start_date != "" && $end_date == "") {
+        $date_condition = "AND date >= '$start_date'";
+} else if ($start_date == "" && $end_date != "") {
+        $date_condition = "AND date <= '$end_date'";
+} else {
+        $date_condition = "AND date >= '$start_date' AND date <= '$end_date'";
+}
+if ($transac_type == 'all') {
+
+}
+
+$sql_stmt = "";
+switch ($transac_type) {
+case 'all':
+$sql_stmt = "SELECT transaction_id, source, recipient, amount, date, time,
+                NULL AS bank_code
+                , DATE_FORMAT(time, '%h:%i %p') AS timef
+        FROM fund_transfer
+        WHERE source='$account_number'
+        $date_condition
+
+        UNION ALL
+        SELECT transaction_id, source, recipient, amount, date, time, bank_code
+                , DATE_FORMAT(time, '%h:%i %p') AS timef
+        FROM fund_transfer_external_send
+        WHERE source='$account_number'
+        $date_condition
+
+        UNION ALL
+        SELECT transaction_id, source, recipient, amount, date, time, bank_code
+                , DATE_FORMAT(time, '%h:%i %p') AS timef
+        FROM fund_transfer_external_receive
+        WHERE recipient='$account_number'
+        $date_condition
+
+        UNION ALL
+        SELECT transaction_id, source, recipient, amount, date, time, 
+                NULL AS bank_code
+                , DATE_FORMAT(time, '%h:%i %p') AS timef
+        FROM fund_transfer
+        WHERE recipient='$account_number'
+        $date_condition
+
+        ORDER BY date DESC, time DESC 
+        LIMIT 10";
+        break;
+case 'internal':
+$sql_stmt = "SELECT transaction_id, source, recipient, amount, date, time,
+                NULL AS bank_code
+                , DATE_FORMAT(time, '%h:%i %p') AS timef
+        FROM fund_transfer
+        WHERE source='$account_number'
+        $date_condition
+
+        ORDER BY date DESC, time DESC 
+        LIMIT 10";
+        break;
+case 'external':
+$sql_stmt = "SELECT transaction_id, source, recipient, amount, date, time, 
+                bank_code, DATE_FORMAT(time, '%h:%i %p') AS timef
+        FROM fund_transfer_external_send
+        WHERE source='$account_number'
+        $date_condition
+
+        ORDER BY date DESC, time DESC 
+        LIMIT 10";
+        break;
+case 'received':
+$sql_stmt = "SELECT transaction_id, source, recipient, amount, date, time, bank_code
+                , DATE_FORMAT(time, '%h:%i %p') AS timef
+        FROM fund_transfer_external_receive
+        WHERE recipient='$account_number'
+        $date_condition
+
+        UNION ALL
+        SELECT transaction_id, source, recipient, amount, date, time, 
+                NULL AS bank_code
+                , DATE_FORMAT(time, '%h:%i %p') AS timef
+        FROM fund_transfer
+        WHERE recipient='$account_number'
+        $date_condition
+
+        ORDER BY date DESC, time DESC 
+        LIMIT 10";
+        break;
+}
+$result = extract_database($sql_stmt);
+$data = array();
+for ($i = 0; $row = mysqli_fetch_assoc($result); $i++) {
+        $type = "External Transfer";
+        $account = $row['recipient'];
+
+        if ($row['bank_code'] == NULL) {
+                $type = "Internal Transfer"; 
+        }
+        if ($row['recipient'] == $account_number) {
+                $type = "Received Transfer";
+                $account = $row['source'];
+        }
+        $name = get_name($account);
+        if (!$name) {
+                $name = get_bank_name($row['bank_code']) . " Account";
+        }
+        $data[$i] = array(
+                'name' => $name,
+                'account' => $account,
+                'transactionID' => $row['transaction_id'],
+                'date' => $row['date'] . " " . $row['timef'],
+                'amount' => '&#8369 ' . $row['amount'],
+                'type' => $type
+        );
+}
+close_database();
+$response['data'] = $data;
+$response['success'] = true;
+$response['start_date'] = $start_date;
+$response['end_date'] = $end_date;
+echo json_encode($response);
+?>
