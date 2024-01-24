8812e5feeebae392e1e72c2ea46427e16646954a
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-01-24 18:35:36 +0800

    fix recent transactions table

--- app/client/backend/php/account-transaction-mini.php
+++ app/client/backend/php/account-transaction-mini.php
@@ -7,56 +7,76 @@ connect_database();
 $account_number = $_SESSION['account_number'];
 
 $sql_stmt = "SELECT
-                source AS account,
+                recipient AS account, source,
                 'Internal Transfer' AS type,
-                amount
+                amount, date, time
             FROM fund_transfer
             WHERE source='$account_number'
 
             UNION ALL
 
             SELECT
-                recipient AS account,
+                source AS account, recipient,
                 'Received Transfer' AS type,
-                amount
+                amount, date, time
             FROM fund_transfer
             WHERE recipient='$account_number'
 
             UNION ALL
 
             SELECT
-                source AS account,
+                source AS account, recipient,
+                'Received Transfer' AS type,
+                amount, date, time
+            FROM fund_transfer_external_receive
+            WHERE recipient='$account_number'
+
+            UNION ALL
+
+            SELECT
+                recipient AS account, source,
                 'External Transfer' AS type,
-                amount
+                amount, date, time
             FROM fund_transfer_external_send
             WHERE source='$account_number'
 
             ORDER BY date DESC, time DESC
-            LIMIT 10";
+            LIMIT 5";
 
 $result = extract_database($sql_stmt);
 
 $response = array();
 
 if (!$result) {
-    // Add error handling here
     $response['error'] = mysqli_error($connection);
     $response['success'] = false;
 } else {
     $data = array();
 
-    while ($row = mysqli_fetch_assoc($result)) {
-        $data[] = array(
-            'account' => $row['account'],
-            'type' => $row['type'],
+    for ($i = 0; $row = mysqli_fetch_assoc($result); $i++) {
+        $name = "";
+        $type = $row['type'];
+        if ($row['type'] == 'External Transfer') {
+            $name = "VRZN Account";
+        }
+        if ($row['type'] == 'Internal Transfer') {
+            $name = get_name($row['account']);
+        }
+        if ($row['type'] == 'Received Transfer') {
+            $name = get_name($row['account']);
+            if (!$name) {
+                $name = " VRZN Account";
+            }
+        }
+        $data[$i] = array(
+            'name' => $name,
+            'type' => $type,
             'amount' => '&#8369 ' . $row['amount'],
         );
     }
-
     close_database();
     $response['data'] = $data;
     $response['success'] = true;
 }
 
 echo json_encode($response);
-?>
