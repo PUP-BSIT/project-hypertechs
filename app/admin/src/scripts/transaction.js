5fe4d5ffa8dce5639a5cfce62d7287cd727172ff
mfckinstarboy <emmanuellmartinez013@gmail.com>
2024-01-25 10:30:17 +0800

    add error modal

--- app/admin/src/scripts/transaction.js
+++ app/admin/src/scripts/transaction.js
@@ -66,3 +66,29 @@ document.addEventListener("DOMContentLoaded", function () {
   let transferContainer = document.querySelector(".apex-transaction-container");
   transferContainer.classList.add("fade-in");
 });
+
+/* Modal settings */
+function openConfirmationPopup() {
+  // Display the modal
+  document.getElementById('confirmationModal').style.display = 'block';
+}
+
+function closeConfirmationPopup() {
+  // Close the modal
+  document.getElementById('confirmationModal').style.display = 'none';
+}
+
+function closeConfirmationPopup() {
+  var confirmationModal = document.getElementById("confirmationModal");
+  var modalContent = document.querySelector(".modal-content");
+
+  // Add the zoom-out class
+  modalContent.classList.add("zoom-out");
+
+  // Wait for the animation to finish before hiding the modal
+  setTimeout(function () {
+    confirmationModal.style.display = "none";
+    // Remove the zoom-out class to reset for the next time
+    modalContent.classList.remove("zoom-out");
+  }, 500); // 500ms should match the animation duration
+}
